97cb807c709fb1a67d9fd48943f2114ee38fa9ed
yukatherin <yu.katherin@gmail.com>
2014-09-09 20:42:37 -0700

    cleaned a few lines

--- prob164.py
+++ prob164.py
@@ -5,8 +5,7 @@ def p164():
     tup_list = list(itertools.product(range(10), range(10), range(10)))
     count_dict = {(i,j,k): 1 if i+j+k<=9 else 0 for i,j,k in tup_list }
     for t in range(17):
-        next_count_dict = {(i,j,k):0 if i+j+k>9 else sum([count_dict[(j,k,x)] for x in range(10)]) for i,j,k in tup_list}
-        count_dict = next_count_dict
+        count_dict = {(i,j,k):0 if i+j+k>9 else sum([count_dict[(j,k,x)] for x in range(10)]) for i,j,k in tup_list}
     tot_sum = sum([count_dict[(i,j,k)] for i,j,k in count_dict if i>0])
     print tot_sum
 
